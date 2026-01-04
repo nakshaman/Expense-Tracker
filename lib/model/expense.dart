bcd12d96537023cb574cf2b7dd00616fa2fb47b0
Aman Kumar <aman58320@gmail.com>
2026-01-04 17:17:24 +0530

    Cleaned yaml file and intialized the files

---
+++ lib/model/expense.dart
@@ -0,0 +1,15 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+class Expense {
+  Expense({
+    required this.date,
+    required this.title,
+    required this.amount,
+  }) : id = uuid.v4();
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
